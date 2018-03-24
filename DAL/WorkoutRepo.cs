29bd021b05dc21e992b3539fb36ab6eab46200d2
primehide <primeservicespp@gmail.com>
2018-03-24 15:32:14 +0100

    DAL repos

---
+++ DAL/WorkoutRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class WorkoutRepo : IWorkoutRepo
+    {
+        private EF.FitnessContext ctx;
+
+        public WorkoutRepo()
+        {
+            ctx = new EF.FitnessContext();
+        }
+    }
+}
