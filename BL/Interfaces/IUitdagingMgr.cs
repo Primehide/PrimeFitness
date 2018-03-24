b3bff31a396e4b81e066b03273ffccbcc2bcc25a
primehide <primeservicespp@gmail.com>
2018-03-24 15:41:57 +0100

    BL managers + UI controllers

--- BL/Interfaces/IUitdagingMgr.cs
+++ BL/Interfaces/IUitdagingMgr.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace BL.Interfaces
 {
-    class GebruikersRepo
+    public interface IUitdagingMgr
     {
     }
 }
---
+++ BL/UitdagingMgr.cs
@@ -0,0 +1,14 @@
+﻿using DAL;
+
+namespace BL
+{
+    public class UitdagingMgr : Interfaces.IUitdagingMgr
+    {
+        private IUitdagingRepo repo;
+
+        public UitdagingMgr()
+        {
+            repo = new UitdagingRepo();
+        }
+    }
+}
