ed57e1cf64055a4636a8097e9e60096dcb324800
primehide <primeservicespp@gmail.com>
2018-03-24 14:42:32 +0100

    Add project files.

---
+++ PrimeFitness/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrimeFitness.Startup))]
+namespace PrimeFitness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
